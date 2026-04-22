81cf50dea8ba458f0e81a7f31e33795c1a09b5cd
Bertin <7010972@spsd.sk.ca>
2026-04-22 15:24:41 -0600

    finished the geotech for turkey

--- Proposal.md
+++ Proposal.md
@@ -3,14 +3,14 @@ For my major project I would like to add to my Geoguessr inspired game. I would 
 
 ##Need to Have
 - global text chat, all players are connected to one chat to communicate
-- custom parties, player creates room code and another player joins
 - custom maps, allows the player to upload custom maps and practice on them
 - satisfying effects, effects to make the game more satisfying like when they click on the map or get a good guess
 - hint system, holds hints for different areas of the world showing them after a guess or saved as a library
+- custom parties, player creates room code and another player joins
 
 ##Nice to Have
-- options for 2V2, 1V2 3V2, etc.
 - a shop for custom pins, also a system to select pins like an inventory
+- options for 2V2, 1V2 3V2, etc.
 - other cosmetic effects. Like a custom particle effect when you place your pin
 - achievments, player titles
 - add expiremental gamemodes, ex. infinite pins mode
--- learn_info.js
+++ learn_info.js
@@ -1,169 +1,260 @@
 let hintedCountries = {
   "Bulgaria": {
     Main: {
-        desc: "BG (Bulgaria) is generally an easy country. It is very mountainous, and they use a greek like language. Also, it does not share many similarities with it's neighbours",
-        name: "Bulgaria (BG)",
-        picture: "GeoTechs/BG/BGMain.png",
-        driving: "Right",
-        plate: "Standard Europe",
-        language: "Bulgarian",
-        difficulty: "4/10"
+      desc: "BG (Bulgaria) is generally an easy country. It is very mountainous, and they use a greek like language. Also, it does not share many similarities with it's neighbours",
+      name: "Bulgaria (BG)",
+      picture: "GeoTechs/BG/BGMain.png",
+      driving: "Right",
+      plate: "Standard Europe",
+      language: "Bulgarian",
+      difficulty: "4/10"
     },
 
     BGRoof: {
-        desc: "Probably the most useful BG tech. Dry bright mud found in the corner of BG roofs are almost always a dead giveaway for BG. Keep in mind that not all houses have these roofs, but most do. Also, parts of other countries might have similar mudding like Turkey",
-        name: "EX: BG Roofing",
-        picture: "GeoTechs/BG/BG-cornerroof.png"
+      desc: "Probably the most useful BG tech. Dry bright mud found in the corner of BG roofs are almost always a dead giveaway for BG. Keep in mind that not all houses have these roofs, but most do. Also, parts of other countries might have similar mudding like Turkey",
+      name: "EX: BG Roofing",
+      picture: "GeoTechs/BG/BG-cornerroof.png"
     },
 
     BGPole: {
-        desc: "Almost exclusive to BG in europe, are these poles with alternating arms perpendicular to the pole. There are a couple variations like some without supports",
-        name: "EX: Alternating Pole",
-        picture: "GeoTechs/BG/BG-altpole.png"
+      desc: "Almost exclusive to BG in europe, are these poles with alternating arms perpendicular to the pole. There are a couple variations like some without supports",
+      name: "EX: Alternating Pole",
+      picture: "GeoTechs/BG/BG-altpole.png"
     },
 
     BGFlag: {
-        desc: "BG flag is shown somewhat often (left flag)",
-        name: "EX: BG flag",
-        picture: "GeoTechs/BG/BG-Flag.png"
+      desc: "BG flag is shown somewhat often (left flag)",
+      name: "EX: BG flag",
+      picture: "GeoTechs/BG/BG-Flag.png"
     },
 
     BGHooks: {
-        desc: "These Hook like insulators are mainly found in western europe. However, BG is the lowest one of them and mainly the only one with mountains",
-        name: "RR: Hook Insulators",
-        picture: "GeoTechs/BG/BG-hooks.png"
+      desc: "These Hook like insulators are mainly found in western europe. However, BG is the lowest one of them and mainly the only one with mountains",
+      name: "RR: Hook Insulators",
+      picture: "GeoTechs/BG/BG-hooks.png"
     },
 
     BGGarbage: {
-        desc: "This metalic bin is found regularily in only a couple eruopean countries, most notably Italy and BG",
-        name: "RR: Metalic Bin",
-        picture: "GeoTechs/BG/BG-Garbagebin.png"
+      desc: "This metalic bin is found regularily in only a couple eruopean countries, most notably Italy and BG",
+      name: "RR: Metalic Bin",
+      picture: "GeoTechs/BG/BG-Garbagebin.png"
     },
 
     BGMountains: {
-        desc: "BG has more mountanous regions that flat ones. Also, for some reason a lot of BG is winter coverage resulting in a lot of dead trees",
-        name: "CC: Mountains",
-        picture: "GeoTechs/BG/BG-mountains.png"
+      desc: "BG has more mountanous regions that flat ones. Also, for some reason a lot of BG is winter coverage resulting in a lot of dead trees",
+      name: "CM: Mountains",
+      picture: "GeoTechs/BG/BG-mountains.png"
     },
 
     BGNumberRoads: {
-        desc: "BG has many numbered roads ranging from mostly 2 to 4 digits. Unfortunately a lot of other european countries also follow this pattern",
-        name: "CC: Numbered Roads",
-        picture: "GeoTechs/BG/BG-numberroad.png"
+      desc: "BG has many numbered roads ranging from mostly 2 to 4 digits. Unfortunately a lot of other european countries also follow this pattern",
+      name: "CM: Numbered Roads",
+      picture: "GeoTechs/BG/BG-numberroad.png"
     },
 
     BGTightRoad: {
-        desc: "a good chunk of BG coverage is on a small road that is surrounded by trees. These trees can range from any height and can often cause you to miss BG. Keep in mind they are no where near exclusive to BG.",
-        name: "CC: Tight Roads",
-        picture: "GeoTechs/BG/BG-tightroad.png"
+      desc: "a good chunk of BG coverage is on a small road that is surrounded by trees. These trees can range from any height and can often cause you to miss BG. Keep in mind they are no where near exclusive to BG.",
+      name: "CM: Tight Roads",
+      picture: "GeoTechs/BG/BG-tightroad.png"
     },
 
     BGThinLines: {
-        desc: "This tech is usually used to tell apart from Romania. Although, me personally I don't think it is nececcary",
-        name: "CC: Thin Lines",
-        picture: "GeoTechs/BG/BG-thinlines.png"
+      desc: "This tech is usually used to tell apart from Romania. Although, me personally I don't think it is nececcary",
+      name: "CM: Thin Lines",
+      picture: "GeoTechs/BG/BG-thinlines.png"
     },
 
     BGBlueCar: {
-        desc: "A ton of BG coverage was taken with a Blue antenna car. Also, found in many other european countries",
-        name: "CC: Blue Antenna",
-        picture: "GeoTechs/BG/BG-blueAnt.png"
+      desc: "A ton of BG coverage was taken with a Blue antenna car. Also, found in many other european countries",
+      name: "CM: Blue Antenna",
+      picture: "GeoTechs/BG/BG-blueAnt.png"
     }
   },
 
   "Greece": {
     Main: {
-        desc: "GR (Greece) is typically a easy country with unique infrastructure, language, and landscape. However, a very difficult of part greece are the islands.",
-        name: "Greece (GR)",
-        picture: "GeoTechs/GR/GR-Main.png",
-        driving: "Right",
-        plate: "Standard Europe",
-        language: "Greek",
-        difficulty: "5/10"
+      desc: "GR (Greece) is typically a easy country with unique infrastructure, language, and landscape. However, a very difficult of part greece are the islands.",
+      name: "Greece (GR)",
+      picture: "GeoTechs/GR/GR-Main.png",
+      driving: "Right",
+      plate: "Standard Europe",
+      language: "Greek",
+      difficulty: "5/10"
     },
 
     GRHousing: {
-        desc: "Southern Greece near Athens feature the most distinct houses. White boxes with blue windows and doors.",
-        name: "EX: Greece Housing",
-        picture: "GeoTechs/GR/GR-Housing.png"
+      desc: "Southern Greece near Athens feature the most distinct houses. White boxes with blue windows and doors.",
+      name: "EX: Greece Housing",
+      picture: "GeoTechs/GR/GR-Housing.png"
     },
 
     GRRoadName: {
-        desc: "Many times GR is free due to it's very unique long road name. It starts with a letter E and is very long. Only ever seen in Greece but not on all roads",
-        name: "EX: Greece Road Name",
-        picture: "GeoTechs/GR/GR-RoadName.png"
+      desc: "Many times GR is free due to it's very unique long road name. It starts with a letter E and is very long. Only ever seen in Greece but not on all roads",
+      name: "EX: Greece Road Name",
+      picture: "GeoTechs/GR/GR-RoadName.png"
     },
 
     GRDoubleWhite: {
-        desc: "Double White lines in europe almost exclusive to Poland and GR. However, the 2 countries look completely different making this an EX tech",
-        name: "EX: Double White",
-        picture: "GeoTechs/GR/GR-WhiteDouble.png"
+      desc: "Double White lines in europe almost exclusive to Poland and GR. However, the 2 countries look completely different making this an EX tech",
+      name: "EX: Double White",
+      picture: "GeoTechs/GR/GR-WhiteDouble.png"
     },
 
     GRLamp: {
-        desc: "This Hunchback lamp is almost exclusive to GR. Hanging downward with poor posture",
-        name: "EX: Hunchback Lamp",
-        picture: "GeoTechs/GR/GR-PoleLamp.png"
+      desc: "This Hunchback lamp is almost exclusive to GR. Hanging downward with poor posture",
+      name: "EX: Hunchback Lamp",
+      picture: "GeoTechs/GR/GR-PoleLamp.png"
     },
 
     GRTownSigns: {
-        desc: "Town Signs in GR are in Greek and feauture yellow and white letters in a blue background. Nowhere else in the world has these",
-        name: "EX: Greek Town Signs",
-        picture: "GeoTechs/GR/GR-TownSigns.png"
+      desc: "Town Signs in GR are in Greek and feauture yellow and white letters in a blue background. Nowhere else in the world has these",
+      name: "EX: Greek Town Signs",
+      picture: "GeoTechs/GR/GR-TownSigns.png"
     },
 
     GRBollards: {
-        desc: "These Special Bollards are red/orange and white. The thing that gives them away are that they are a perfect square reflector. However, they are notably rare",
-        name: "EX: Greek Bollards",
-        picture: "GeoTechs/GR/GR-Bollard.png"
+      desc: "These Special Bollards are red/orange and white. The thing that gives them away are that they are a perfect square reflector. However, they are notably rare",
+      name: "EX: Greek Bollards",
+      picture: "GeoTechs/GR/GR-Bollard.png"
     },
 
     GRMiniChurches: {
-        desc: "Probably one of my favourite Geotecs are the mini churches. I have only ever seen them in GR and are pretty common aswell",
-        name: "EX: Mini Church",
-        picture: "GeoTechs/GR/GR-MiniChurches.png"
+      desc: "Probably one of my favourite Geotecs are the mini churches. I have only ever seen them in GR and are pretty common aswell",
+      name: "EX: Mini Church",
+      picture: "GeoTechs/GR/GR-MiniChurches.png"
     },
 
     GRDarkPoles: {
-        desc: "Most poles in GR are wooden and for some reason feature this darker colour. But I would not fully rely on this",
-        name: "RR: Dark Poles",
-        picture: "GeoTechs/GR/GR-DarkPole.png"
+      desc: "Most poles in GR are wooden and for some reason feature this darker colour. But I would not fully rely on this",
+      name: "RR: Dark Poles",
+      picture: "GeoTechs/GR/GR-DarkPole.png"
     },
 
     GRHarpPoles: {
-        desc: "This is what I would call the Harp pole. It is mainly found in greece but variations can be found in Turkey",
-        name: "RR: Harp Pole",
-        picture: "GeoTechs/GR/GR-HarpPole.png"
+      desc: "This is what I would call the Harp pole. It is mainly found in greece but variations can be found in Turkey",
+      name: "RR: Harp Pole",
+      picture: "GeoTechs/GR/GR-HarpPole.png"
     },
 
     GRMountains: {
-        desc: "Massive Mountains are found all over Greece, but be careful as the north is suprisingly flat in some areas",
-        name: "RR: Huge Mountains",
-        picture: "GeoTechs/GR/GR-MassiveMountains.png"
+      desc: "Massive Mountains are found all over Greece, but be careful as the north is suprisingly flat in some areas",
+      name: "RR: Huge Mountains",
+      picture: "GeoTechs/GR/GR-MassiveMountains.png"
     },
 
     GRCheverons: {
-        desc: "GreGRece uses single white on black cheverons. Also, found most notably in Italy and Albania",
-        name: "RR: White Cheverons",
-        picture: "GeoTechs/GR/GR-Chevrons.png"
+      desc: "GreGRece uses single white on black cheverons. Also, found most notably in Italy and Albania",
+      name: "RR: White Cheverons",
+      picture: "GeoTechs/GR/GR-Chevrons.png"
     },
 
     GRShrubs: {
-        desc: "Lots of GR mountains are covered with these shrubbish like plants",
-        name: "CC: Shrubbish Plants",
-        picture: "GeoTechs/GR/GR-ShrubbishPlants.png"
+      desc: "Lots of GR mountains are covered with these shrubbish like plants",
+      name: "CM: Shrubbish Plants",
+      picture: "GeoTechs/GR/GR-ShrubbishPlants.png"
     },
   },
 
   "Turkey": {
     Main: {
-        desc: "Turk (Turkey) is a very easy country between europe and Asia. It is easily recognized but can be tricky on some rural rounds.",
-        name: "Turkey (Turk)",
-        picture: "GeoTechs/GR/GR-Main.png",
-        driving: "Right",
-        plate: "Standard Europe",
-        language: "Turkish",
-        difficulty: "3/10"
-    }
+      desc: "Turk (Turkey) is a very easy country between europe and Asia. It is easily recognized but can be tricky on some rural rounds.",
+      name: "Turkey (Turk)",
+      picture: "GeoTechs/GR/GR-Main.png",
+      driving: "Right",
+      plate: "Standard Europe",
+      language: "Turkish",
+      difficulty: "3/10"
+    },
+
+    TurkBollards: {
+      desc: "One of the reasons why Turk is insanely easy, are their unique exclusive simple looking bollards. Keep in mind that scross the world Australia has similar ones, and I have seen new players mix them up",
+      name: "EX: Turkish Bollards",
+      picture: "GeoTechs/Turk/Turk-Bollard.png"
+    },
+    
+    TurkMosqueTower: {
+      desc: "A dead giveaway to Turk are their skinny tall Mosques. Mainly only seen in Turk and high enough to see from anywhere",
+      name: "EX: Mosque Tower",
+      picture: "GeoTechs/Turk/Turk-Towers.png"
+    },
+
+    TurkSkinnyHolePole: {
+      desc: "Unique to Turk are these super skinny Holey Poles",
+      name: "EX: Skinny Holey Poles",
+      picture: "GeoTechs/Turk/Turk-SkinnyHolePole.png"
+    },
+
+    TurkStopSigns: {
+      desc: "Unique to Turk are DUR stop signs",
+      name: "EX: DUR Stop Signs",
+      picture: "GeoTechs/Turk/Turk-StopSigns.png"
+    },
+
+    TurkLanguage: {
+      desc: "Turkish is a somewhat unique language. It is a slavic language with double dots over U and O. Also, with tails under C",
+      name: "EX: Turkish Language",
+      picture: "GeoTechs/Turk/Turk-Slavic.png"
+    },
+
+    TurkYolu: {
+      desc: "some Turkish roads end with Yolu, and I have only seen this in Turkey",
+      name: "EX: Yolu Roads",
+      picture: "GeoTechs/Turk/Turk-Yolu.png"
+    },
+
+    TurkWaterTanks: {
+      desc: "Metal water tanks are found all over Turkey. Keep in mind that there are a lot of different types of water tanks",
+      name: "EX: Metal Water Tanks",
+      picture: "GeoTechs/Turk/Turk-WaterTank.png"
+    },
+
+    TurkHousing: {
+      desc: "Housing in Turk are typically very tall. This is unique compared to a lot of other European countries",
+      name: "RR: Tall Housing",
+      picture: "GeoTechs/Turk/Turk-TallHousing.png"
+    },
+
+    TurkHighways: {
+      desc: "Divided highways are rare in Europe and Turkey has many wide divided highways",
+      name: "RR: Wide Highways",
+      picture: "GeoTechs/Turk/Turk-DividedHighways.png"
+    },
+
+    TurkLargeMesh: {
+      desc: "Tall metal mesh towers are very common in Turkey but also found in other European countries like Port and Spain",
+      name: "RR: Mesh Towers",
+      picture: "GeoTechs/Turk/Turk-LargeMeshedPole.png"
+    },
+
+    TurkSignPost: {
+      desc: "90%+ of all sign posts have an indent in Turk. Especially on their red cheverons. This is found all over the world (like Canada) but not as common in Europe",
+      name: "RR: Post Indent",
+      picture: "GeoTechs/Turk/Turk-Chevs.png"
+    },
+
+    TurkPepperRoading: {
+      desc: "Many Rural small roads in Turk have these peppered roads. Rare to find in Europe but common in the world",
+      name: "RR: Peppered Roading",
+      picture: "GeoTechs/Turk/Turk-PepperRoad.png"
+    },
+
+    TurkDRoads: {
+      desc: "Europe is covered in single digit roads like France and Crotia, but also Turk",
+      name: "CM: D Roading",
+      picture: "GeoTechs/Turk/Turk-Droads.png"
+    },
+
+    TurkMountains: {
+      desc: "Like a lot of Europe Turk is covered in mountains. The vegatation can be used to region guess at times, and it is rare to find a flat Turk round",
+      name: "CM: Mountains",
+      picture: "GeoTechs/Turk/Turk-Mountains.png"
+    },
+
+    TurkCheverons: {
+      desc: "Many european coutries use this red on white cheveron",
+      name: "CM: Cheverons",
+      picture: "GeoTechs/Turk/Turk-Chevs.png"
+    },
+
   },
 };
